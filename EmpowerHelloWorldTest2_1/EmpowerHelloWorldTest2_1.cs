90cef9b25b361e904a87c6ddffcadceffcd59bfb
Gelber <gelber.mahecha@skyline.be>
2023-06-01 10:18:06 -0300

    Recovered codes

--- EmpowerHelloWorldTest2_1/EmpowerHelloWorldTest2_1.cs
+++ EmpowerHelloWorldTest2_1/EmpowerHelloWorldTest2_1.cs
@@ -68,7 +68,7 @@ namespace EmpowerHelloWorldTest2_1
         /// <param name="engine">Link with SLAutomation process.</param>
         public void Run(IEngine engine)
         {
-            engine.GenerateInformation("Hello");
+            engine.GenerateInformation("Hello"); 
         }
         public void OperationA(IEngine engine, int count)
         {
