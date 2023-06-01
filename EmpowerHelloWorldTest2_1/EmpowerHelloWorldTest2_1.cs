46819f6bad01ca9b50755614c2d7fcf963dc7f3c
Gelber <gelber.mahecha@skyline.be>
2023-06-01 11:30:54 -0300

    Avoid any Engine run.

--- EmpowerHelloWorldTest2_1/EmpowerHelloWorldTest2_1.cs
+++ EmpowerHelloWorldTest2_1/EmpowerHelloWorldTest2_1.cs
@@ -68,7 +68,7 @@ namespace EmpowerHelloWorldTest2_1
         /// <param name="engine">Link with SLAutomation process.</param>
         public void Run(IEngine engine)
         {
-            engine.GenerateInformation("Hello"); 
+           //  engine.GenerateInformation("Hello"); 
         }
 		public int  OperationA(int count)
 		{
