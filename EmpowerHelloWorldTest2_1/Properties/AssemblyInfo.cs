dfd6203e4acf518e52a42f52b6471209787185fb
Gelber <gelber.mahecha@skyline.be>
2023-06-01 07:40:12 -0300

    Add project files.

---
+++ EmpowerHelloWorldTest2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerHelloWorldTest2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerHelloWorldTest2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("763B550C-BDB1-4659-9D65-590045EF0508")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,1 @@
+# EmpowerHelloWorldTest2
