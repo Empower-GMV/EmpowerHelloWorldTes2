5e85ffc14801bb8e84a8a142cb1bc670315d7dbb
Gelber <gelber.mahecha@skyline.be>
2023-06-01 10:49:28 -0300

    Added unittests

---
+++ UnitTestProjectHelloWorld2/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProjectHelloWorld2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var script = new EmpowerHelloWorldTest2_1.Script();
+            var result = script.OperationA(10);
+            Assert.AreEqual(100, result);
+            result = script.OperationB(10);
+            Assert.AreEqual(1000, result);
+
+        }
+    }
+}
